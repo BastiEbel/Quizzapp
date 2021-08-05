4f1e2c3d644a89466d348717d3c8483a3703f2b8
Sebastian Ebel <sebastianebel@hotmail.de>
2021-08-05 20:37:50 +0200

    added JSON and Question Box

---
+++ script.js
@@ -0,0 +1,50 @@
+let questions = [
+    {
+        'question': 'Wer hat HTML erfunden?',
+        'answer1': 'Robbie Williams',
+        'answer2': 'Lady Gaga',
+        'answer3': 'Tim Berners-Lee',
+        'answer4': 'Justin Bieber',
+        'right_answer': 3
+    },
+    {
+        'question': 'Was bedeutet das HTML Tag &lt;a&gt;?',
+        'answer1': 'Text Fett',
+        'answer2': 'Container',
+        'answer3': 'Ein Link',
+        'answer4': 'Kursiv',
+        'right_answer': 3
+    },
+    {
+        'question': 'Wie bindet man eine Webseite in eine Webseite ein?',
+        'answer1': '&lt;iframe&gt;, &lt;fram&gt;, and &lt;frameset&gt;',
+        'answer2': '&lt;iframe&gt;',
+        'answer3': '&lt;frame&gt;',
+        'answer4': '&lt;frameset&gt;',
+        'right_answer': 2
+    },
+    {
+        'question': 'Wie stellt man Text am BESTEN fett dar?',
+        'answer1': '&lt;strong&gt;',
+        'answer2': 'css nutzen',
+        'answer3': '&lt;bold&gt;',
+        'answer4': '&lt;b&gt;',
+        'right_answer': 1
+    },
+    {
+        'question': 'Wie wählst du alle Elemente vom Typ &lt;a&gt; mit dem Attribut Title aus?',
+        'answer1': 'Robbie Williams',
+        'answer2': 'Lady Gaga',
+        'answer3': 'Tim Berners-Lee',
+        'answer4': 'Justin Bieber',
+        'right_answer': 1
+    },
+    {
+        'question': 'Wie definiert man in JavaScript eine Variable?',
+        'answer1': 'let 100 = rate;',
+        'answer2': '100 = let rate;',
+        'answer3': 'rate = 100;',
+        'answer4': 'let rate = 100;',
+        'right_answer': 4
+    }
+];
