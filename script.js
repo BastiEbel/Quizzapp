3a313c5e16f8ecc0256641fc018c6ffd963cdced
Sebastian Ebel <sebastianebel@hotmail.de>
2021-08-08 10:27:14 +0200

    init question right or wrong

--- script.js
+++ script.js
@@ -49,11 +49,11 @@ let questions = [
     }
 ];
 
-let currentQuestion = 0;
+let currentQuestion = 0;  //variable erstellen die auf 0 gesetzt wird
 
 //load function
-function init(){
-    document.getElementById('number').innerHTML = questions.length;
+function init() {
+    document.getElementById('number').innerHTML = questions.length; //ladet die Anzahl der gesamten Fragen und gibt sie aus.
 
     showquestion();
 }
@@ -61,14 +61,48 @@ function init(){
 function showquestion() {
     let question = questions[currentQuestion];
 
-    document.getElementById('questiontext').innerHTML = question['question'];
+    document.getElementById('questiontext').innerHTML = question['question']; //fragt die Frage ab und zeigt sie an
 
-    document.getElementById('answer1').innerHTML = question['answer1'];
+    document.getElementById('answer1').innerHTML = question['answer1']; //fragt die Antworten ab und zeigt sie an
     document.getElementById('answer2').innerHTML = question['answer2'];
     document.getElementById('answer3').innerHTML = question['answer3'];
     document.getElementById('answer4').innerHTML = question['answer4'];
 }
 
-function answer(selectedBTN) {
-    
+function answer(selectedBTN) {  //definierte onclick Funktion selectedBTN ruft die id des geklickten Elements ab.
+    let question = questions[currentQuestion];
+
+    let rightAnswer = `answer${question['right_answer']}`;
+
+    let questionanswer = selectedBTN.slice(-1); // das letzte Zeichen ('Char') wird vom string abgefragt.
+
+    if (questionanswer == question['right_answer']) { // Abfrage ob die Antwort richtig oder falsch ist
+        document.getElementById(selectedBTN).classList.add('question-right');
+    } else {
+        document.getElementById(selectedBTN).classList.add('question-wrong');
+        document.getElementById(rightAnswer).classList.add('question-right');
+    }
+
+    document.getElementById('next-button').disabled = false; //Button wird Enabled
+}
+
+function nextQuestion() {
+    currentQuestion++; //wird von z.b. 0 auf 1 erhöht usw.
+
+    document.getElementById('next-button').disabled = true; //Button wird Disabled
+
+    removeButtons();
+
+    showquestion();
+}
+
+function removeButtons() {
+    document.getElementById('answer1').classList.remove('question-right');
+    document.getElementById('answer1').classList.remove('question-wrong');
+    document.getElementById('answer2').classList.remove('question-right');
+    document.getElementById('answer2').classList.remove('question-wrong');
+    document.getElementById('answer3').classList.remove('question-right');
+    document.getElementById('answer3').classList.remove('question-wrong');
+    document.getElementById('answer4').classList.remove('question-right');
+    document.getElementById('answer4').classList.remove('question-wrong');
 }
